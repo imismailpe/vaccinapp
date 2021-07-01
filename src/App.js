e595f99dd3bfed848ff600f30726d1e80838f241
Ismail <mailismailpe@gmail.com>
2021-07-01 15:40:34 +0530

    free and paid colors

--- src/App.js
+++ src/App.js
@@ -12,84 +12,111 @@ function App() {
   const [centerList, setCenterList] = useState([]);
   const [district, setDistrict] = useState('');
   const [loading, setLoading] = useState(false);
+  const [loadingDist, setLoadingDist] = useState(false);
+  const [timer, setTimer] = useState(0);
+  let timerObj;
   useEffect(() => {
-    setLoading(true);
+    setLoadingDist(true);
     axios.get(`${APIBASEURL}${DISTRICTLISTENDPOINT}${KERALANUMBER}`)
       .then(resp => {
-        setDistrictList(resp.data.districts);
-        setLoading(false);
+        setDistrictList(resp.data.districts || []);
+        setLoadingDist(false);
+        resp.data.districts && resp.data.districts.length > 0 && setDistrict(resp.data.districts[0].district_id);
       })
       .catch(e => {
         alert("error:", e)
-        setLoading(false);
+        setLoadingDist(false);
       })
   }, []);
   useEffect(() => {
+    getData();
+  }, [district]);
+  // useEffect(() => {
+  //   if (timer === 0) {
+  //     // console.log("timer is zero")
+  //     // clearTimeout(timerObj);
+  //     getData();
+  //   }
+  //   if(timer > 0){
+  //     // timerObj = setTimeout(handleTimer, 1000);
+  //   }
+  // }, [timer]);
+  const getData = () => {
     setLoading(true);
     const dateInput = moment().format('DD-MM-YYYY');
     district && axios.get(`${APIBASEURL}${DATABYDISTRICTENDPOINT}${district}&date=${dateInput}`)
       .then(resp => {
         setCenterList(resp.data.centers);
         setLoading(false);
+        // setTimer(5000);
+        // handleTimer();
       })
       .catch(e => {
         alert("error:", e);
         setLoading(false);
       })
-  }, [district]);
+  }
+  const handleTimer = () => {
+    setTimer(timer - 1000);
+  }
   return (
     <div className="App">
-      <div>Select a district to see Covid vaccine slot details</div>
-      <div>
+      <div>Select your district to see Covid vaccine slots</div>
+      <div className='distList'>
         {
-          loading === true ? 'Loading...'
+          loadingDist === true ? 'Loading...'
             : <>
               {
                 districtList.length > 0 ?
-                <div className='distList'>
-                  {districtList.map(districtItem => {
-                    return (
-                      <div className={districtItem.district_id === district ? 'chatItem selectedDist': 'chatItem'} key={districtItem.district_id} onClick={() => setDistrict(districtItem.district_id)}>{districtItem.district_name}</div>
-                    )
-                  })}
+                  <div className=''>
+                    {districtList.map(districtItem => {
+                      return (
+                        <div className={districtItem.district_id === district ? 'chatItem selectedDist' : 'chatItem'} key={districtItem.district_id} onClick={() => setDistrict(districtItem.district_id)}>{districtItem.district_name}</div>
+                      )
+                    })}
                   </div>
                   : 'No Districts available'
               }
-              <div className='chatWindow'>
-                {
-                  centerList.length > 0 ? centerList.map(center => {
-                    return (
-                      <div key={center.center_id} className='messageInBubble'>
-                        <div className='centerName'>{center.name}-{center.block_name}</div>
-                        <div className='centerFee'>{center.fee_type}</div>
-                        <div className='centerAddress'>{center.address} - {center.pincode}</div>
-                        {
-                          center.sessions.length > 0 && center.sessions.map(session => {
-                            return (
-                              <div key={session.session_id} className={session.available_capacity ? 'availableBG sessionContainer' : 'notAvailableBG sessionContainer'}>
-                                <div className='vaccineName'>{session.vaccine}</div>
-                                <div className='vaccineDate'>{session.date}</div>
-                                <div className='vaccineCapacity'>Available capacity: {session.available_capacity}, Dose1: {session.available_capacity_dose1}, Dose2: {session.available_capacity_dose2}</div>
-                                <div className='slotContainer'>
-                                {
-                                  session.slots.map(slot => {
-                                    return (
-                                      <div key={slot} className='slotTime'>{slot}</div>
-                                    )
-                                  })
-                                }
-                                </div>
-                              </div>
-                            )
-                          })
-                        }
-                      </div>
-                    )
-                  })
-                    : 'No Centers are available for the selected district. Refresh after some time.'
-                }
-              </div>
             </>
+        }
+      </div>
+      {/* <div>Data will refresh in {timer / 1000} seconds.</div> */}
+      <div className='chatWindow'>
+        {
+          loading === true ? 'Loading...'
+            :
+            centerList.length > 0 ? centerList.map(center => {
+              return (
+                <div key={center.center_id} className='messageInBubble'>
+                  <div className='centerTitle'>
+                  <div className='centerName'>{center.name}-{center.block_name}</div>
+                  <div className={center.fee_type === 'Free' ? 'greenText centerFee' : 'redText centerFee'}>{center.fee_type}</div>
+                  </div>
+                  <div className='centerAddress'>{center.address} - {center.pincode}</div>
+                  {
+                    center.sessions.length > 0 && center.sessions.map(session => {
+                      return (
+                        <div key={session.session_id} className={session.available_capacity ? 'availableBG sessionContainer' : 'notAvailableBG sessionContainer'}>
+                          <div className='vaccineName'>{session.vaccine}</div>
+                          <div className='vaccineDate'>{session.date}</div>
+                          <div className='vaccineCapacity'>Available capacity: {session.available_capacity}, Dose1: {session.available_capacity_dose1}, Dose2: {session.available_capacity_dose2}</div>
+                          <div className='slotContainer'>
+                            {
+                              session.slots.map(slot => {
+                                return (
+                                  <div key={slot} className='slotTime'>{slot}</div>
+                                )
+                              })
+                            }
+                          </div>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              )
+            })
+              : 'No Centers are available for the selected district. Refresh after some time.'
         }
       </div>
     </div>
